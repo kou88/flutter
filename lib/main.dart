327573a563e8f1ef2acb03af77489d1134f504c4
kou88 <kou.move.8@gmail.com>
2021-02-23 20:58:49 +0900

    mod color blue to orange.

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
@@ -76,6 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title, textAlign: TextAlign.left),
+        elevation: 0, // shadow off under the app bar.
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
