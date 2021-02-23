a35beed1a39407d63437675dd854048b1aa6ea81
kou88 <kou.move.8@gmail.com>
2021-02-23 22:49:29 +0900

    Add scroll.

--- lib/main.dart
+++ lib/main.dart
@@ -89,43 +89,46 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width * 0.7,
-              child: Image.asset('images/logo_transparent.png'),
-            ),
-            Container(
-                width: MediaQuery.of(context).size.width * _textboxnWidthRatio,
-                child: TextField()),
-            Container(
-              // Set the ratio to the screen size
-              width: MediaQuery.of(context).size.width * _buttonWidthRatio,
-              // リスト追加ボタン
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('検索', style: TextStyle(color: Colors.black)),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.grey[200],
+        child: SingleChildScrollView(
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: Image.asset('images/logo_transparent.png'),
+              ),
+              Container(
+                  width:
+                      MediaQuery.of(context).size.width * _textboxnWidthRatio,
+                  child: TextField()),
+              Container(
+                // Set the ratio to the screen size
+                width: MediaQuery.of(context).size.width * _buttonWidthRatio,
+                // リスト追加ボタン
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('検索', style: TextStyle(color: Colors.black)),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.grey[200],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
